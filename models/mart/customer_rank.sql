e4d965cdb367d604562638d97c16a7b5b7ca56f3
asankobasakit@gmail.com <asankobasakit@gmail.com>
2022-06-08 06:34:10 +0000

    change

--- models/mart/customer_rank.sql
+++ models/mart/customer_rank.sql
@@ -1,1 +1,6 @@
-select 1 where 1=2
+select C.CUST_ID, COUNt(distinct O.Order_Id) AS TOTAL_ORDER_CNT, sum(ORDER_AMT) AS TOTAL_ORDER_VAL,
+RANK() OVER(ORDER BY TOTAL_ORDER_VAL DESC ) RN 
+from {{ref('stg_customer')}} C join 
+{{ ref('stg_orders')}} O
+on C.CUST_ID=O.CUST_ID
+group by C.CUST_ID ORDER BY 3 DESC
--- models/mart/customer_rank_nationwise.sql
+++ models/mart/customer_rank_nationwise.sql
@@ -1,1 +1,12 @@
-select 1 where 1=2
+SELECT CUST_ID, CUST_NAME, CTRY_NAME, RN,TotalOrderValue 
+FROM (
+    select LC.CUST_ID, LC.CUST_NAME, LN.CTRY_NAME,SUM(LO.ORDER_AMT) as TotalOrderValue ,
+    RANK() over(partition by LN.CTRY_NAME order by TotalOrderValue desc) RN
+    from {{ref('stg_customer')}} LC 
+    join {{ ref('stg_orders')}}  LO
+    on LC.CUST_ID=LO.ORDER_ID
+    join {{ ref('stg_nation')}}  LN
+    on LC.CTRY_ID=LN.CTRY_ID
+    group by LC.CUST_ID,LC.CUST_NAME,LN.CTRY_NAME
+    order by CTRY_NAME, RN 
+) WHERE RN<=5
