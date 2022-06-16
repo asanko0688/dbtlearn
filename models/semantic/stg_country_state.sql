b74132937881b8de5f9b9ccadc9482f8e787f871
asankobasakit@gmail.com <asankobasakit@gmail.com>
2022-06-16 05:27:44 +0000

    chnages

---
+++ models/semantic/stg_country_state.sql
@@ -0,0 +1,10 @@
+{{
+  config(
+    materialized = "incremental"
+  )
+}}
+
+SELECT COUNTRY, STATE, CODE,
+md5(STATE) AS HASH_VAL,
+CURRENT_TIMESTAMP AS LOADDATETIME
+FROM {{ source('CRM', 'country_state') }} 
